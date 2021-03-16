9bf03620edb1ce08ec67dc410c1dbe36b1c14ec6
Gilberto Alvarez Gamboa <gilbertoalvarez514@hotmail.com>
2021-03-15 17:19:08 -0700

    se complementa cuerto de la pagina, se agrega video, se estructura para tamano tablet

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 function mostrar() {
     document.getElementById("menu-vertical").style.display = "inline";
-    /*document.getElementById("menu-vertical").style.width = "80%";
-    document.getElementsByClassName("lista-menu").style.width = "80%";*/
+
 }
 function ocultar() {
     document.getElementById("menu-vertical").style.display = "none";
-    /*document.getElementById("menu-vertical").style.width = "0";
-    document.getElementsByClassName("lista-menu").style.width = "0";*/
   
 }
